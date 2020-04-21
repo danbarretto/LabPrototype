74008b92226286aba94c1f0a503482355e1ff741
danielbarretto <daniel_barretto98@hotmail.com>
2020-04-20 21:49:07 -0300

    First Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // Start is called before the first frame update
+    public string xAxis, zAxis, fire;
+
+    public float speed = 20f;
+    private Rigidbody rigidBody;
+
+    // Update is called once per frame
+    private void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void Update() {
+        float moveHorizontal = Input.GetAxis(xAxis);
+        float moveVertical = Input.GetAxis(zAxis);
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        if (movement != Vector3.zero) {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+
+        rigidBody.velocity = new Vector3(moveHorizontal * speed, 0f, moveVertical * speed);
+
+        if(Input.GetButtonDown(fire)){
+            Debug.Log("Ação");
+        }
+
+    }
+}
